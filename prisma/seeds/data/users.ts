46b6ddb7255c20f9b0a47450036f5fa01bd5f49e
David Hernandez <david.hernandez.v44@gmail.com>
2025-06-10 11:56:20 -0600

    init

--- prisma/seeds/data/users.ts
+++ prisma/seeds/data/users.ts
@@ -1,7 +1,7 @@
 import { ConfigService } from '@nestjs/config';
-import { CryptoService } from '../../../common/application/services/crypto.service';
+import { CryptoService } from '../../../src/common/application/services/crypto.service';
 import { ConfigEnv } from 'src/common/domain/types/env.type';
-import { UsersBuilder } from '../../../users/domain/builders/users.builder';
+import { UsersBuilder } from '../../../src/users/domain/builders/users.builder';
 
 const cryptoService = new CryptoService(new ConfigService<ConfigEnv, true>());
 
